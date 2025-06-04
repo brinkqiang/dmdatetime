c8e8d5c9251e5824b657742ed3971e44319a11aa
brinkqiang <brink.qiang@gmail.com>
2025-06-04 23:00:21 +0800

    add

--- include/dmdatetime.h
+++ include/dmdatetime.h
@@ -334,7 +334,7 @@ public:
 
     static CDMDateTime MaxValue() {
         if (sizeof(std::time_t) > 4) {
-            return CDMDateTime(3000, 12, 1, 23, 59, 59);
+            return CDMDateTime(2800, 1, 1, 1, 1, 1);
         }
         else {
             return CDMDateTime(2038, 1, 19, 3, 14, 7);
